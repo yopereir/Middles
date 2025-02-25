eaef9e37f3db160565a8780a77a390b923657d4c
Yohann Pereira <ypereira@bgea.org>
2025-02-25 16:08:25 -0500

    updated go server

--- go/test-server/images/meme-generator-server/server.go
+++ go/test-server/images/meme-generator-server/server.go
@@ -8,6 +8,7 @@ import (
 	"math/rand"
     "net/http"
 	"strings"
+    "time"
 	"encoding/json"
     "github.com/filipkroca/revgeo"
 	"context"
@@ -62,31 +63,87 @@ func getMemes(query string, country string) (Meme){
 }
 
 func getSubscriptionTypeAndDeductBalance(bearerToken string) (string, error) {
-    response, err := http.Get("https://"+TokenManagerURL+"?token="+bearerToken)
-    if err != nil || response.StatusCode != http.StatusOK {
-        return "", err
-    }
-	defer response.Body.Close()
-    var result map[string]string
-    if err:= json.NewDecoder(response.Body).Decode(&result); err !=nil {
-        return "", err
-    }
-    return result["subscriptionType"], nil
-}
-
-func main() {
 	// Init Redis
 	ctx := context.Background()
 	redisClient := redis.NewClient(&redis.Options{Addr: "redis:6379",})
     _, err := redisClient.Ping(ctx).Result()
-    if err != nil {log.Printf("Error Connecting to redis: %s\n", err)} else {log.Println("Successfully connected to Redis!")}
+    if err != nil {
+        log.Printf("Error Connecting to redis: %s\n", err)
+        return "", err
+    } else {
+        log.Println("Successfully connected to Redis!")
+    }
 	defer redisClient.Close()
 
 	// Init Postgres
 	dbClient, err := sqlx.Connect("postgres", "host=postgres port=5432 user="+POSTGRES_USER+" password="+POSTGRES_PASSWORD+" dbname="+POSTGRES_DB+" sslmode=disable")
-    if err != nil {log.Printf("Error Connecting to Postgres: %s\n", err)} else {log.Println("Successfully connected to Postgres!")}
+    if err != nil {
+        log.Printf("Error Connecting to Postgres: %s\n", err)
+        return "", err
+    } else {
+        log.Println("Successfully connected to Postgres!")
+    }
 	defer dbClient.Close()
 
+    // Get User Data
+    // Get userID 
+    //  response, err := http.Get("https://"+TokenManagerURL+"?token="+bearerToken)
+    //  if err != nil || response.StatusCode != http.StatusOK {return "", err}
+	//  defer response.Body.Close()
+    //  var result map[string]string
+    //  if err:= json.NewDecoder(response.Body).Decode(&result); err !=nil {return "", err}
+    //  userID := result["userId"]
+    userID := "user1" // Mock data
+
+    // Get currentBalance
+    var currentBalance int
+    // Check if userBalance is present on Redis
+	userBalance, err := redisClient.Get(ctx, "token:"+bearerToken).Result()
+    if err == redis.Nil {
+        // Get userBalance from Postgres if not present in Redis
+        var fetchedBalance int
+        err := dbClient.QueryRow("SELECT tokenBalance FROM users WHERE userID = $1", userID).Scan(&fetchedBalance)
+        if err != nil {return "", err}
+        userBalance = fmt.Sprintf("%d", fetchedBalance)
+        // Set Balance in Redis
+        err = redisClient.Set(ctx, "token:"+bearerToken, userBalance, time.Hour).Err()
+        if err != nil {return "", err}
+    }
+    currentBalance, err = strconv.Atoi(userBalance)
+    if err != nil {return "", err}
+
+    // Get subscriptionType
+    // Check if subscriptionType is present on Redis
+	subscriptionType, err := redisClient.Get(ctx, "subscription:"+bearerToken).Result()
+    if err == redis.Nil {
+        // Get subscriptionType from Postgres if not present in Redis
+        var fetchedSubscriptionType string
+        err := dbClient.QueryRow("SELECT subscriptionType FROM users WHERE userID = $1", userID).Scan(&fetchedSubscriptionType)
+        if err != nil {return "", err}
+        subscriptionType = fetchedSubscriptionType
+        // Set Balance in Redis
+        err = redisClient.Set(ctx, "subscription:"+bearerToken, subscriptionType, time.Hour).Err()
+        if err != nil {return "", err}
+    }
+    
+    // Deduct Balance
+    if subscriptionType == "AI" {
+        currentBalance -= 3
+    } else {
+        currentBalance -= 1
+    }
+    // Deduct Balance in redis
+    err = redisClient.Set(ctx, "token:"+bearerToken, userBalance, time.Hour).Err()
+    if err != nil {return subscriptionType, err}
+    // Deduct Balance from Postgres asynchronously
+    go func () {
+        _, err = dbClient.Exec("UPDATE users SET tokenBalance = $1 where userID = $2",currentBalance,userID)
+        if err != nil {log.Printf("Error updating the Postgres database with user data:\nuserID: "+userID+"\ntokenBalance: "+fmt.Sprintf("%d",currentBalance))}
+    }()
+    return subscriptionType, nil
+}
+
+func main() {
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "This is the meme generation server")
     })

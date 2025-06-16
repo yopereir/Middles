812d12305de949e73db030fa6159409fc46f6066
Yohann Pereira <ypereira@bgea.org>
2025-06-16 16:28:14 -0400

    updated momentum trading script

--- python/trading-momemtum-arbitrage/test.py
+++ python/trading-momemtum-arbitrage/test.py
@@ -12,6 +12,7 @@ TICKER = os.getenv('ALPACA_TICKER', 'SATS')
 LIMIT = os.getenv('ALPACA_LIMIT', '1')
 EXPIRATION_DATE="2025-06-20"
 STRIKE_PRICE=17
+OPTIONS_SYMBOL = "SRPT250620C00036500"
 
 headers = {
     "accept": "application/json",
@@ -21,5 +22,6 @@ headers = {
 
 # SETUP STOCK PRICE REQUEST
 url = f"https://data.alpaca.markets/v1beta1/options/snapshots/{TICKER}?feed={FEED}&limit=2&strike_price_gte={STRIKE_PRICE}&strike_price_lte={STRIKE_PRICE}&expiration_date_lte={EXPIRATION_DATE}"
+url = f"https://data.alpaca.markets/v1beta1/options/quotes/latest?symbols={OPTIONS_SYMBOL}&feed={FEED}"
 response = requests.get(url, headers=headers)
 print(response.json())

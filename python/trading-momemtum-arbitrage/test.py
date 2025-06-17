57c2ae0b6fc77bf17744cfd78b672c7e441f8315
Yohann Pereira <ypereira@bgea.org>
2025-06-17 11:13:08 -0400

    updated momentum trading script

--- python/trading-momemtum-arbitrage/test.py
+++ python/trading-momemtum-arbitrage/test.py
@@ -1,9 +1,10 @@
-import os, requests
+import os, requests, json
 from dotenv import load_dotenv
 
 # LOAD ENVIRONMENT VARIABLES
 load_dotenv()
 ALPACA_URL = os.getenv('ALPACA_ACCOUNT_URL')
+ALPACA_BASE_URL = os.getenv('ALPACA_BASE_URL')
 ALPACA_KEY = os.getenv('ALPACA_API_KEY')
 ALPACA_SECRET = os.getenv('ALPACA_API_SECRET')
 ALPHA_VANTAGE_KEY = os.getenv('ALPHA_VANTAGE_KEY')
@@ -12,17 +13,28 @@ TICKER = os.getenv('ALPACA_TICKER', 'SATS')
 LIMIT = os.getenv('ALPACA_LIMIT', '1')
 EXPIRATION_DATE="2025-06-20"
 STRIKE_PRICE=17
+
 OPTIONS_SYMBOL = "RUN250620C00010000"
+ALPACA_BASE_URL = "https://paper-api.alpaca.markets/v1beta1"
 
 headers = {
     "accept": "application/json",
+    "content-type": "application/json",
     "APCA-API-KEY-ID": ALPACA_KEY,
     "APCA-API-SECRET-KEY": ALPACA_SECRET
 }
 
+payload = {
+    "symbol": OPTIONS_SYMBOL,
+    "qty": 1,
+    "side": "sell",                # "buy" or "sell"
+    "type": "limit",               # "market", "limit", "stop", etc.
+    "limit_price": 0.01,           # Premium to be collected for the option
+    "time_in_force": "day",        # "day", "gtc", etc.
+    "order_class": "simple",       # Only "simple" supported for now
+    "legs": None                   # Required only for multi-leg orders
+}
+
 # SETUP STOCK PRICE REQUEST
-url = f"https://data.alpaca.markets/v1beta1/options/quotes/latest?symbols={OPTIONS_SYMBOL}&feed=indicative"
-response = requests.get(url, headers=headers)
-optionQuoteDetails = response.json()['quotes'][OPTIONS_SYMBOL]
-print(abs(optionQuoteDetails['bp'] + optionQuoteDetails['ap'])/2)
+response = requests.post(f"{ALPACA_BASE_URL}/options/orders", headers=headers, data=json.dumps(payload))
 print(response.json(), response.status_code)
